nxp-qoriq/tsntool

commit 40965a1fb855b3a23b2430eb83e5a3973da04cd9

tsntool: add Qbv gate list method from command line
Current Qbv gate list input from editor or file input. This new method
to add gate list on command line follow up the parameter --entryfile.
Define as:
	[t<X>/<GATE BIT>/CYCLE]
Compatible with the file method but with '[]' and '/' seperate them.

Example:
qbvset --enable --device swp3 \
--entryfile [t0/00001111b/50000][t1/00010000b/60000]

Signed-off-by: Po Liu <po.liu@nxp.com>

main/cmd.c
@@ -584,8 +584,9 @@ int cli_cmd_qbv_set(UNUSED int argc, UNUSED char *argv[], UNUSED int cmdnumber)
 			break;
 		case 'f':
 			if (access(optarg, R_OK) != 0) {
-				ERROR("setqbv", "There isn't a local file (%s).", strerror(errno));
-				return -1;
+				/* for further parser */
+				config = strdup(optarg);
+				break;
 			}
 			config_fd = open(optarg, O_RDONLY);
 			if (config_fd == -1) {

main/fill.c
@@ -120,7 +120,7 @@ void qbv_fill_one_entry(struct tsn_qbv_entry *conf, uint32_t num, uint8_t gate, 
 				(conf + num), (conf + num)->gate_state, (conf + num)->time_interval);
 }
 
-int qbv_entry_parse(char *config, struct tsn_qbv_entry *conf, uint32_t *count, uint32_t *cycletime)
+static int qbv_entry_parse_from_file(char *config, struct tsn_qbv_entry *conf, uint32_t *count, uint32_t *cycletime)
 {
 	char *delim = "\n";
 	char *pch;
@@ -180,6 +180,73 @@ int qbv_entry_parse(char *config, struct tsn_qbv_entry *conf, uint32_t *count, u
 
 		pch = strtok(NULL, delim);
 	}
+
+	return 0;
+}
+
+static int qbv_entry_parse_from_para(char *config, struct tsn_qbv_entry *conf, uint32_t *count, uint64_t *cycletime)
+{
+	char *delim = "]";
+	char *pch;
+	uint32_t number;
+	char state[10];
+	uint32_t time;
+	int gate;
+
+	*count = 0;
+
+	pch = strtok(config, delim);
+
+	while (pch != NULL) {
+
+		while ((*pch == ' ') || (*pch == '\t') || (*pch == '['))
+			pch++;
+
+		if (*pch == '#') {
+			pch = strtok(NULL, delim);
+			continue;
+		}
+
+		if ((*pch == 'T') || (*pch == 't')) {
+			sscanf((pch + 1), "%d/%9s/%d", &number, state, &time);
+
+			logv("\n qbv entry: number: %d		state: %s		time: %d\n", number, state, time);
+
+			if (number < *count) {
+				logv("ERROR: Duplicate number.\n");
+				return -1;
+			}
+
+			if (number >= MAX_ENTRY_SIZE) {
+				logv("ERROR: larger than max entry number");
+				return -1;
+			}
+
+			if (!time) {
+				logv("WARNING: time period should not be zero.\n");
+				pch = strtok(NULL, delim);
+				continue;
+			}
+
+			//sscanf(st, "%[0-1]", state);
+
+			gate = qbv_str2gate(state);
+			if (gate < 0) {
+				logv("gate value is not valid for entry number T%d.\n", number);
+				return -1;
+			}
+
+			qbv_fill_one_entry(conf, *count, (uint8_t)gate, time);
+
+			(*cycletime) += (uint64_t)time;
+			(*count)++;
+		}
+
+		pch = strtok(NULL, delim);
+	}
+
+	if (*count == 0)
+		return -1;
 
 	return 0;
 }
@@ -254,10 +321,14 @@ int fill_qbv_set(char *portname, char *config, bool enable, uint8_t configchange
 
 	memset(conf, 0, MAX_ENTRY_SIZE);
 
-	ret  = qbv_entry_parse(config, conf, &count, &cycletime);
+	ret = qbv_entry_parse_from_para(config, conf, &count, &cycletime);
 	if (ret < 0) {
-		free(conf);
-		return -1;
+		count = 0; cycletime = 0;
+		ret  = qbv_entry_parse_from_file(config, conf, &count, &cycletime);
+		if (ret < 0) {
+			free(conf);
+			return -1;
+		}
 	}
 
 	adminconf.admin.cycle_time = cycletime;

commit 4564a9c66e49d0931a08b9d87d83bae86a5d70f4

tsntool: fix qbv config warning
When setting Qbv from entry file, there will be a warning:
	"no gate[0] value, less than 8 gates"

This is because we don't distinguish whether Qbv configuration comes from
a file or the command line. This patch adds a bool to select different
function to set Qbv, this can avoid the warning.

Fixes: 40965a1 ("tsntool: add Qbv gate list method from command line")
Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

main/cmd.c
@@ -574,6 +574,7 @@ int cli_cmd_qbv_set(UNUSED int argc, UNUSED char *argv[], UNUSED int cmdnumber)
 	int option_index = 0;
 	char portname[IF_NAMESIZE];
 	int ret;
+	bool isfilein = 0;
 	uint32_t maxsdu = 0;
 	uint8_t configchange = 0;
 	uint64_t basetime = 0;
@@ -622,6 +623,8 @@ int cli_cmd_qbv_set(UNUSED int argc, UNUSED char *argv[], UNUSED int cmdnumber)
 
 			munmap(config_m, config_stat.st_size);
 			close(config_fd);
+
+			isfilein = 1;
 
 			//logv("entries setting:\n%s\n", config);
 
@@ -713,6 +716,7 @@ int cli_cmd_qbv_set(UNUSED int argc, UNUSED char *argv[], UNUSED int cmdnumber)
 			ERROR("setqbv", "read entries failed.");
 			return -1;
 		}
+		isfilein = 0;
 	}
 
 	if (!device) {
@@ -722,7 +726,7 @@ int cli_cmd_qbv_set(UNUSED int argc, UNUSED char *argv[], UNUSED int cmdnumber)
 		return -1;
 	}
 
-	if (fill_qbv_set(portname, config, disable ? 0:1,
+	if (fill_qbv_set(portname, config, isfilein, disable ? 0:1,
 				configchange, basetime,
 				cycletime, cycletimeext, maxsdu, initgate) != 0) {
 		if (config)

main/fill.c
@@ -279,9 +279,9 @@ int fill_qci_cap_get(char *portname)
 	return tsn_qci_streampara_get(portname, &sp);
 }
 
-int fill_qbv_set(char *portname, char *config, bool enable, uint8_t configchange,
-		uint64_t basetime, uint32_t cycletime,
-		uint32_t cycletimeext, uint32_t maxsdu, uint8_t initgate)
+int fill_qbv_set(char *portname, char *config, bool isfilein, bool enable,
+		 uint8_t configchange, uint64_t basetime, uint32_t cycletime,
+		 uint32_t cycletimeext, uint32_t maxsdu, uint8_t initgate)
 {
 	uint32_t count = 0;
 	struct tsn_qbv_entry *conf = NULL;
@@ -321,14 +321,15 @@ int fill_qbv_set(char *portname, char *config, bool enable, uint8_t configchange
 
 	memset(conf, 0, MAX_ENTRY_SIZE);
 
-	ret = qbv_entry_parse_from_para(config, conf, &count, &cycletime);
-	if (ret < 0) {
+	if (isfilein) {
 		count = 0; cycletime = 0;
 		ret  = qbv_entry_parse_from_file(config, conf, &count, &cycletime);
 		if (ret < 0) {
 			free(conf);
 			return -1;
 		}
+	} else {
+		ret = qbv_entry_parse_from_para(config, conf, &count, &cycletime);
 	}
 
 	adminconf.admin.cycle_time = cycletime;

main/fill.h
@@ -6,9 +6,9 @@
 #ifndef __END_DEF_FILL_H__
 #define __END_DEF_FILL_H__
 int fill_tsn_cap_get(char *portname);
-int fill_qbv_set(char *portname, char *config, bool enable, uint8_t configchange,
-		uint64_t basetime, uint32_t cycletime,
-		uint32_t cycletimeext, uint32_t maxsdu, uint8_t initgate);
+int fill_qbv_set(char *portname, char *config, bool isfilein, bool enable,
+		 uint8_t configchange, uint64_t basetime, uint32_t cycletime,
+		 uint32_t cycletimeext, uint32_t maxsdu, uint8_t initgate);
 int fill_qbv_get(char *portname);
 int fill_qci_cap_get(char *portname);
 